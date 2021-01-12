e1b260571fa13a2887a0b8b73513ef0a761d9ae9
Roger Deetz <rdeetz@gmail.com>
2021-01-12 11:56:34 -0500

    Added initial project.

---
+++ QUnoUniversal/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QUno")]
+[assembly: AssemblyDescription("An Uno-like card game.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Roger Deetz")]
+[assembly: AssemblyProduct("QUno")]
+[assembly: AssemblyCopyright("Copyright (c) 2021 Roger Deetz. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: ComVisible(false)]
