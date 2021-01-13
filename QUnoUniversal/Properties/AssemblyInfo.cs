822a70c59b5933c59dcc8eddc864963683669a24
Roger Deetz <rdeetz@gmail.com>
2021-01-13 12:16:50 -0500

    Updated copyright.

--- QUnoUniversal/Properties/AssemblyInfo.cs
+++ QUnoUniversal/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Roger Deetz")]
 [assembly: AssemblyProduct("QUno")]
-[assembly: AssemblyCopyright("Copyright (c) 2021 Roger Deetz. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2019-2021 Roger Deetz. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("5.0.0.0")]
